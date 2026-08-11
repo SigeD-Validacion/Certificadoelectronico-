19a5cb0d2beef5c7f5190900bff759997032f14e
SigeD-Validacion <sugeeysaliinasso5@gmail.com>
2026-08-11 14:30:59 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCrLDSRloFeTdoJFT8hahdbMfCP468NDIw",
+  authDomain: "certificadoelectronico-89f1e.firebaseapp.com",
+  projectId: "certificadoelectronico-89f1e",
+  storageBucket: "certificadoelectronico-89f1e.firebasestorage.app",
+  messagingSenderId: "1071784580481",
+  appId: "1:1071784580481:web:4d53c299ef597acf6f0480"
+};
